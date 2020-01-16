c1bde74341af8b55268d585faff021159596ecc5
Constantin Prokhorov <53078494+ConstantinProkhorov@users.noreply.github.com>
2020-01-16 17:42:53 +0300

    Update Level_2_Controller.cs

--- Assets/Scripts/SceneControllers/GameLevelControllers/Level_2_Controller.cs
+++ Assets/Scripts/SceneControllers/GameLevelControllers/Level_2_Controller.cs
@@ -1,16 +1,4 @@
-<<<<<<< Updated upstream
-﻿//public class Level_2_Controller : LevelSceneController
-//{
-//    public Level_2_Controller() : base(/*buildindex =*/ 8, /*LevelName =*/ "GameLevel 2") { }
-//    new void Start()
-//    {
-//        base.Start();
-//    }
-//}
 using System;
-=======
-﻿using System;
->>>>>>> Stashed changes
 using UnityEngine.UI;
 public class Level_2_Controller : LevelSceneController
 {
