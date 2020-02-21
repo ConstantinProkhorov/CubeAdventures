b7fb7c0783c72ce688a0e7bead7f7d6fdbe283fe
Constantin Prokhorov <53078494+ConstantinProkhorov@users.noreply.github.com>
2020-02-21 12:55:19 +0300

    ground work complete

---
+++ Assets/Scripts/Enemy Logic/DelayedBehaviourActivation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+
+public class DelayedBehaviourActivation : MonoBehaviour
+{
+    // поведение, которое надо запустить с отсрочкой //
+    [SerializeField] private Behaviour Behaviour;
+    // значение отсрочки в секундах
+    [SerializeField] private float DelayTime = 2.0f;
+    // таймер
+    [SerializeField] private TimeCounter TimeCounter;
+
+    void Start()
+    {
+        TimeCounter.TimerFinshed += delegate (object sender, EventArgs eventArgs)
+        {
+            Behaviour.enabled = true;
+        };
+        TimeCounter.TurnOn(DelayTime);
+    }
+}
