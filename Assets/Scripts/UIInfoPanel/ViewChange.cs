5cf7f3fe9ca7ed0729a61a4e861a07ca0790a2ad
Constantin Prokhorov <53078494+ConstantinProkhorov@users.noreply.github.com>
2020-03-29 18:40:14 +0300

    added commentary

--- Assets/Scripts/UIInfoPanel/ViewChange.cs
+++ Assets/Scripts/UIInfoPanel/ViewChange.cs
@@ -6,5 +6,8 @@ public abstract class ViewChange : MonoBehaviour
     [TextArea]
     public string OptionalDescription = "Enter an optional description for this component.\nThis is for convinience only.";
     [SerializeField] protected Text Display;
+    /// <summary>
+    /// Displays the value passed as parameter. Exact way the value is displayed is determined in child classes of ViewChange.
+    /// </summary>
     public abstract void Change(int value);
 }
